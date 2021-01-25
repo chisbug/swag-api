008eeccd6a5f635f961304a1f37e50ef5b820a1f
Ch <303918108@qq.com>
2021-01-25 13:48:07 +0800

    fix: update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-  .version('0.0.5', '-v, --version')
+  .version('0.0.6', '-v, --version')
   .command('create <url> <path>')
   .action(require('./src/create'));
 
